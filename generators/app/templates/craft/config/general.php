be032c96b6baffd24133c13e49aee0d2af2f2cd5
Tyrel Witcher <tyrel.witcher@gmail.com>
2021-12-21 10:29:26 -0600

    Editing craft configuration to combine things and remove settings that aren't needed anymore.

--- generators/app/templates/craft/config/general.php
+++ generators/app/templates/craft/config/general.php
@@ -10,69 +10,60 @@
 
 use craft\helpers\App;
 
+$isDev = App::env('ENVIRONMENT') === 'dev';
+$isProd = App::env('ENVIRONMENT') === 'production';
+
 return [
     // Global settings
     '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 0,
 
-        // Whether generated URLs should omit "index.php"
-        'omitScriptNameInUrls' => true,
+        // Whether admins should be allowed to make administrative changes to the system.
+        'allowAdminChanges' => $isDev,
 
-        //T he default options that should be applied to each search term.
+        // Whether Craft should create a database backup before applying a new system update.
+        'backupOnUpdate' => true,
+
+        // Control panel trigger word
+        'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
+
+        // The default options that should be applied to each search term.
         'defaultSearchTermOptions' => array(
             'subLeft' => true,
             'subRight' => true,
         ),
 
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 0,
+
+        // Whether the system should run in Dev Mode.
+        'devMode' => $isDev,
+
+        // Whether crawlers should be allowed to index pages and following links
+        'disallowRobots' => !$isProd,
+
         // Whether to enable CSRF protection via hidden form inputs for all forms submitted via Craft.
         'enableCsrfProtection' => true,
-
-        // Control panel trigger word
-        'cpTrigger' => 'admin',
-
-        // The secure key Craft will use for hashing and encrypting data
-        'securityKey' => App::env('SECURITY_KEY'),
-
-        // Whether admins should be allowed to make administrative changes to the system.
-        'allowAdminChanges' => false,
 
         // The prefix that should be prepended to HTTP error status codes when determining the path to look for an error’s template.
         'errorTemplatePrefix' => '_errors/_',
 
-        // Whether Craft should set users’ usernames to their email addresses, rather than let them set their username separately.
-        'useEmailAsUsername' => true,
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
 
-        // List of file extensions that will be merged into the allowedFileExtensions config setting.
-        'extraAllowedFileExtensions' => ['json'],
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => App::env('SECURITY_KEY'),
 
-        // Whether Craft should allow system and plugin updates in the control panel, and plugin installation from the Plugin Store.
-        'allowUpdates' => false,
-
-        // Whether Craft should create a database backup before applying a new system update.
-        'backupOnUpdate' => false,
-
-        // Whether the system should run in Dev Mode.
-        'devMode' => false,
-
-        // Whether admins should be allowed to make administrative changes to the system.
-        'allowAdminChanges' => false
     ],
 
     // Dev environment settings
     'dev' => [
-        'allowUpdates' => true,
-        'allowAdminChanges' => true,
-        'backupOnUpdate' => true,
-        'devMode' => true,
         'enableTemplateCaching' => false,
         'userSessionDuration' => 0
     ],
 
     // Staging environment settings
     'staging' => [
-        // Prevent crawlers from indexing pages and following links
-        'disallowRobots' => true,
+        'devMode' => true,
     ],
 
     // Production environment settings
